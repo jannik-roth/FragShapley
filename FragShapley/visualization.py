9fcc8b6c9ace147c8cd4b9800e57638e82cba6ab
Jannik P. Roth <jannikproth@gmail.com>
2026-03-24 16:24:11 +0100

    Updates visualization and README.md

--- FragShapley/visualization.py
+++ FragShapley/visualization.py
@@ -5,6 +5,129 @@ from rdkit.Chem import Draw, MolFromSmiles
 from IPython.display import SVG
 from collections import defaultdict
 
+import matplotlib as mpl
+import matplotlib.cm as cm
+from itertools import combinations
+
+def visualize_contributions_new(smiles,
+                                frag_to_atom_ids, 
+                                contributions, 
+                                min=-5,
+                                max=-5,
+                                colormap="bwr",
+                                rad=0.3,
+                                legend='',
+                                center=None):
+    '''
+        Visualizes the contribution of with circles. Uses rdKit. Draw to create a SVG. Contributions are highlighted using circles of different colours, the radius scaling accoringly to the contribution
+
+        input:
+            smiles: str, SMILES of the molecule
+            contributions: np.array, array containing the contributions per atom
+            scaler: float, controls the scaling of the radius of the circles drawn. Needs to be adjusted accordingly
+    '''
+
+    return visualize_contributions_from_mol_new(MolFromSmiles(smiles), frag_to_atom_ids,contributions, min, max, colormap, rad, legend, center)
+
+def visualize_contributions_from_mol_new(mol,
+                                         frag_to_atom_ids, 
+                                         contributions, 
+                                         min=-5,
+                                         max=-5,
+                                         colormap="bwr",
+                                         rad=0.3,
+                                         legend='',
+                                         center=None):
+    
+
+    atomHighlights = defaultdict(list)
+    bondHighlights = defaultdict(list)
+    atomRads = {}
+
+    if center is not None:
+        norm = mpl.colors.TwoSlopeNorm(vcenter=center, vmin=min, vmax=max)
+    else:
+        norm = mpl.colors.Normalize(vmin=min, vmax=max)
+    cmap = cm.get_cmap(colormap)
+    m = cm.ScalarMappable(norm=norm, cmap=cmap)
+    colors = [m.to_rgba(value) for value in contributions]
+
+    for (fragment ,color) in zip(frag_to_atom_ids.values(),colors):
+        for aid in fragment:
+            atomHighlights[aid].append(color)
+            atomRads[aid] = rad
+        
+        for a, b in combinations(fragment, 2):
+            bnd = mol.GetBondBetweenAtoms(a,b)
+            if bnd is not None:
+                bondHighlights[bnd.GetIdx()].append(color)
+
+    d2d = Draw.MolDraw2DSVG(-1, -1)
+    dopts = d2d.drawOptions()
+    dopts.useBWAtomPalette()
+    # we need to set the highlights to be circles or we'll end up with ovals
+    # that fit around the atomic symbol
+    dopts.atomHighlightsAreCircles = True
+
+    # we need to provide highlightBonds=[] here to avoid having the bonds between highlighted atoms highlighted:
+    # d2d.DrawMolecule(mol,
+    #                  highlightAtoms=dict(atomHighlights),
+    #                  highlightAtomColors=colors,
+    #                  highlightAtomRadii=rad,
+    #                  highlightBonds=dict(bondHighlights),
+    #                  legend=legend)
+    d2d.DrawMoleculeWithHighlights(mol,legend,dict(atomHighlights),dict(bondHighlights),atomRads,{})
+    d2d.FinishDrawing()
+    return SVG(d2d.GetDrawingText())
+
+def visualize_atom_contributions_from_mol_new(mol,
+                                             atom_contributions, 
+                                             min=-5,
+                                             max=-5,
+                                             colormap="bwr",
+                                             rad=0.3,
+                                             legend='',
+                                             center=None):
+        
+
+    atomHighlights = defaultdict(list)
+    #bondHighlights = defaultdict(list)
+    atomRads = {}
+
+    
+    if center is not None:
+        norm = mpl.colors.TwoSlopeNorm(vcenter=center, vmin=min, vmax=max)
+    else:
+        norm = mpl.colors.Normalize(vmin=min, vmax=max)
+
+    cmap = cm.get_cmap(colormap)
+    m = cm.ScalarMappable(norm=norm, cmap=cmap)
+    #colors = [m.to_rgba(value) for value in atom_contributions]
+
+
+    atomHighlights = {i: [m.to_rgba(value)] for i, value in enumerate(atom_contributions)}
+    atomRads = {i: rad for i, _ in enumerate(atom_contributions)}
+        
+
+    d2d = Draw.MolDraw2DSVG(-1, -1)
+    dopts = d2d.drawOptions()
+    dopts.useBWAtomPalette()
+    # we need to set the highlights to be circles or we'll end up with ovals
+    # that fit around the atomic symbol
+    dopts.atomHighlightsAreCircles = True
+
+    # we need to provide highlightBonds=[] here to avoid having the bonds between highlighted atoms highlighted:
+    # d2d.DrawMolecule(mol,
+    #                  highlightAtoms=dict(atomHighlights),
+    #                  highlightAtomColors=colors,
+    #                  highlightAtomRadii=rad,
+    #                  highlightBonds=dict(bondHighlights),
+    #                  legend=legend)
+    d2d.DrawMoleculeWithHighlights(mol,legend,dict(atomHighlights),{},atomRads,{})
+    d2d.FinishDrawing()
+    return SVG(d2d.GetDrawingText())
+
+
 def visualize_contributions(smiles, 
                             contributions, 
                             scale=0.5,
@@ -67,6 +190,9 @@ def visualize_contributions_from_mol(mol,
                      legend=legend)
     d2d.FinishDrawing()
     return SVG(d2d.GetDrawingText())
+
+
+
 
 def get_atom_contribution_from_result_dict(smiles, results_dict, frag_to_atom_ids):
     '''
--- README.md
+++ README.md
@@ -13,3 +13,50 @@ This repository contains the code for the publication "Explanations for Molecula
     - `final_figures/` contains the final figures used in the publication (assembled externally with a vector graphics editor)
 - `requirements.txt` is the file created by running `pip freeze > requirements.txt` with the venv activated. Can be used to create a new virtual environment.
 - `FragShapley/` contains all the code to run the fragment-based Shapley value approach introduced in the publication.
+
+## Usage
+
+The usage of the provided code for the generation of fragemnt-level Shapley values is demonstrated in `3_solubility/gcn_regression.py` (regression, GCN), `3_solubility/rf_regression.py` (regression, RF) and `4_mutagenicity/gcn_classification.py` (classification, GCN) and `4_mutagenicity/rf_classification.py`. 
+
+Here is a code snippet for a scikit-learn model:
+```python
+  import FragShapley
+  # requires
+  # model (here scikit-learn model), already trained
+  # mfpgen FingerprintGenerator64 object from rdkit.Chem.rdFingerprintGenerator.GetMorganGenerator
+  # expected_value, can be "empty" for empty fingerprint or graph or float
+  
+  
+  frag_explainer = FragShapley.FragmentExplainer(model=best_regr,
+                                                 fingerprint_generator=mfpgen,
+                                                 fragmentation_method='BRICS',
+                                                 expected_value=expected_value)
+  ev_frag = frag_explainer.expected_value # get expected value
+
+  # obtain explanations
+  results_dicts, frag_to_atom_ids, atom_id_to_bits = frag_explainer.explain(smiles_test, return_atom_id_to_bits=True)
+```
+
+Here is an example for a GCN:
+```python
+  import Lightning as L
+  import FragShapley
+  # requires
+  # model, trained GCN model with the function get_empy_graph to obtain an empty graph
+  trainer = L.Trainer(accelerator='auto')
+  expected_value = 'empty' # can also be float
+  batch_size = 8
+  featurizer = FragShapley.Featurizer(input_format='smiles', output_format='graph')
+
+  frag_explainer = FragShapley.FragmentExplainer(model=model,
+                                                 expected_value=expected_value,
+                                                 fragmentation_method='BRICS',
+                                                 representation='graph',
+                                                 trainer=trainer,
+                                                 featurizer=featurizer,
+                                                 batch_size=batch_size,)
+  ev_frag = frag_explainer.expected_value # get expected value
+  
+  # obtain explanations
+  results_dicts, frag_to_atom_ids = frag_explainer.explain(smiles_test, return_atom_id_to_bits=False)
+```
